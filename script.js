8049a34c05f9923f5fe8a2937d0726b19d86526c
QianxueSno <qzhao01@risd.edu>
2021-05-05 21:05:18 -0400

    Revert "1"
    This reverts commit a3dca09e30ad2eed4671bf233f7fe61af2cf8bb1.

--- script.js
+++ script.js
@@ -100,11 +100,4 @@ $(document).ready(function(){
 // Function to generate random number
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
-
-		//
-		<script>
-		$(document).ready(function(){
-			$('[data-toggle="tooltip"]').tooltip();
-		});
-		</script>
 }
